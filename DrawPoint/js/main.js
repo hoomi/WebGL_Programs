e8ec2c61b0bfdd6b435acdf07f5cd751e3f8f224
hoomi <hoomi2010@gmail.com>
2013-12-03 19:22:39 +0000

    Wokring version of drawing a point

--- DrawPoint/js/main.js
+++ DrawPoint/js/main.js
@@ -1,3 +1,10 @@
+var VERTEX_SHADER = "void main() {\n" +
+                    "gl_Position = vec4(0.5,-0.5,0.0,1.0);\n" + 
+                    "gl_PointSize = 10.0;\n" +
+                    "}",
+    FRAGMENT_SHADER = "void main() {\n" + 
+                      "gl_FragColor = vec4(1.0,0.0,0.0,1.0);\n" +
+                      "}";
 function main() {
   console.debug("main function");
   var canvas,
@@ -9,6 +16,12 @@ function main() {
     return;
   }
 
+  if (!initShaders(webGLContext,VERTEX_SHADER,FRAGMENT_SHADER)) {
+    console.error("Error in initilizing the shaders");
+    return;
+  }
   webGLContext.clearColor(0.0,0.0,0.0,1.0);
   webGLContext.clear(webGLContext.COLOR_BUFFER_BIT);
+
+  webGLContext.drawArrays(webGLContext.POINTS, 0, 1);
 }
