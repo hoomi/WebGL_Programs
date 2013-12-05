57c8419de66590729550ff4d8ce277f6975a6872
hoomi <hoomi2010@gmail.com>
2013-12-05 17:17:00 +0000

    Random color points were added to the trail

--- DrawPoint/js/main.js
+++ DrawPoint/js/main.js
@@ -5,8 +5,10 @@ function WebGL(canvasId) {
                     "gl_Position = point_position;\n" + 
                     "gl_PointSize = point_size;\n" +
                     "}",
-    FRAGMENT_SHADER = "void main() {\n" + 
-                      "gl_FragColor = vec4(1.0,0.0,0.0,1.0);\n" +
+    FRAGMENT_SHADER = "precision mediump float;\n" +
+                      "uniform vec4 u_FragColor;\n" +
+                      "void main() {\n" + 
+                      "gl_FragColor = u_FragColor;\n" +
                       "}",
     $canvas = $("#" + canvasId ),
     that = this,
@@ -34,8 +36,10 @@ function WebGL(canvasId) {
   this.gl = getWebGLContext(this.canvas,DEBUG);
   this.pointPosition = -1;
   this.pointSize = -1;
+  this.pointColor = -1;
   this.intervalPointer = -1;
   this.pointsArray = [];
+  this.colorsArray =[];
   if (!this.gl) {
     console.error("Could not get the WebGL context");
     return undefined;
@@ -47,12 +51,13 @@ function WebGL(canvasId) {
   try {
     this.pointPosition = this.gl.getAttribLocation(this.gl.program,"point_position");
     this.pointSize = this.gl.getAttribLocation(this.gl.program,"point_size");
+    this.pointColor = this.gl.getUniformLocation(this.gl.program,"u_FragColor");
   } catch (e) {
     console.error("Exception occured when tryng to get the attributes from GLSL");
     return undefined;
   }
 
-  if (this.pointPosition < 0 || this.pointSize < 0 ) {
+  if (this.pointPosition < 0 || this.pointSize < 0 || this.pointColor < 0) {
     console.error("Wrong attribute was returned");
     return undefined;
   }
@@ -60,23 +65,29 @@ function WebGL(canvasId) {
   $canvas.mousemove(canvasClick);
   this.gl.clearColor(0.0,0.0,0.0,1.0);
   this.draw = function(x, y) {
-    that.pointsArray.push(x);
-    that.pointsArray.push(y);
+    var randomMultiplier = Math.random(1);
+    that.pointsArray.push([x,y]);
+    if (that.colorsArray.length <= 200) {
+      that.colorsArray.push([Math.abs(randomMultiplier*x),Math.abs(randomMultiplier * y),Math.abs(x*y*randomMultiplier),1.0]);
+    }
     startInterval();
   };
 
   this.drawArray = function() {
-    //To have mirroring put this value to be 1
-    var subtractor = 2;
+    var xy,rgba;
     that.gl.clear(that.gl.COLOR_BUFFER_BIT);
-    for (var i = that.pointsArray.length - 1; i >= 0; i -= subtractor) {
-      that.gl.vertexAttrib3f(that.pointPosition,that.pointsArray[i-1],that.pointsArray[i],0.0);
+    for (var i = that.pointsArray.length - 1; i >= 0; i --) {
+      xy = that.pointsArray[i];
+      rgba = that.colorsArray[i];
+      that.gl.vertexAttrib3f(that.pointPosition,xy[0],xy[1],0.0);
       that.gl.vertexAttrib1f(that.pointSize,20.0);
+      that.gl.uniform4f(that.pointColor,rgba[0],rgba[1],rgba[2],rgba[3]);
       that.gl.drawArrays(that.gl.POINTS,0,1);
     }
-    that.pointsArray.splice(0,2);
+    that.pointsArray.splice(0,1);
     if (that.pointsArray.length <= 0) {
       stopInterval();
+      that.colorsArray = [];
     }
   };
 
